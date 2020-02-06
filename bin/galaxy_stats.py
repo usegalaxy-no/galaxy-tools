474a2a6627847606b56b517522cc15e03a743f11
brugger <kim.brugger@uib.no>
2020-02-06 12:46:45 +0100

    added code for users and data upload, time for testing

--- bin/galaxy_stats.py
+++ bin/galaxy_stats.py
@@ -11,6 +11,8 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 import argparse
 
+import datetime
+
 
 import kbr.db_utils as db_utils
 import kbr.args_utils as args_utils
@@ -46,21 +48,6 @@ def get_job_stats(day:int=None, hour:int=None):
     if total > 0:
         print("jobs,{}state={}\tcount={}".format(timeframe,"total", total))
 
-
-
-
-def queue_overview(last_hours:int=None, last_days:int=None):
-    q =  "SELECT  tool_id, tool_version, state, destination_id AS destination, job_runner_name, create_time, update_time FROM job "
-    q += " WHERE state = 'running' OR state = 'queued' OR state = 'new' "
-
-    if last_hours is not None:
-        q += "AND update_time > now() - INTERVAL '{} hour' ".format(last_hours)
-    elif last_days is not None:
-        q += "AND update_time > now() - INTERVAL '{} day' ".format(last_days)
-
-    print( q )
-
-
 def stats_jobs(args):
     if len( args.command) == 0:
         get_job_stats()
@@ -85,6 +72,107 @@ def stats_jobs(args):
         sys.exit()
 
 
+def get_user_stats(year:int=None,month:str=None):
+
+    # default we show for the current month
+    today = datetime.datetime.today()
+    datem = datetime.datetime(today.year, today.month, 1)
+    where = "WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-{}-01'::date ".format( today.year, today.month )
+
+    # or show for a full year
+    if year is not None:
+        where="WHERE date_trunc('year', job.create_time AT TIME ZONE 'UTC') = '{}-01-01'::date ".format( year)
+    # or a given month (year-month)
+    elif month is not None:
+        where="WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-01'::date ".format( month )
+
+    q  = "SELECT date_trunc('month', job.create_time AT TIME ZONE 'UTC')::date as month, "
+    q += "count(distinct user_id) as active_users FROM job {}".format( where )
+    q += "GROUP BY month ORDER BY month DESC"
+
+    print( q )
+
+    for entry in db.get_as_dict( q ):
+        print("active-users,timeframe=month,date={}\tcount={}".format(timeframe,entry['state'], entry['count']))
+        total += int(entry['count'])
+
+    if total > 0:
+        print("jobs,{}state={}\tcount={}".format(timeframe,"total", total))
+
+
+def stats_users(args):
+    if len( args.command) == 0:
+        get_user_stats()
+        return
+
+    commands = ['year', 'month', 'help']
+    command = args.command.pop( 0 )
+    args_utils.valid_command(command, commands)
+
+    if command == 'year':
+        args_utils.count(1, len(args.command), msg="stats year require a year")
+        get_user_stats(year=args.command.pop(0))
+    elif command == 'month':
+        args_utils.count(1, len(args.command), msg="stats month require a year-month")
+        get_user_stats(month=args.command.pop(0))
+    else:
+        print("stats users sub-commands: {}".format(", ".join(commands)))
+        sys.exit()
+
+
+
+def get_upload_stats(month:int=None,day:int=None,hour:int=None):
+
+    q  = "SELECT coalesce(sum(dataset.total_size), 0) as size FROM job "
+    q += "LEFT JOIN job_to_output_dataset ON job.id = job_to_output_dataset.job_id "
+    q += "LEFT JOIN history_dataset_association ON "
+    q += " job_to_output_dataset.dataset_id = history_dataset_association.id "
+    q += "LEFT JOIN dataset ON history_dataset_association.dataset_id = dataset.id "
+    q += "WHERE job.tool_id = 'upload1'"
+
+    timeframe = "hour"
+    size      = 1
+    if month is not None:
+        timeframe = "month"
+        size = offset = args_utils.get_or_default(args.command, 1)
+    elif day is not None:
+        timeframe = "day"
+        size = offset = args_utils.get_or_default(args.command, 1)
+    elif hour is not None:
+        timeframe = "hour"
+        size = offset = args_utils.get_or_default(args.command, 1)
+
+    q += "AND job.create_time AT TIME ZONE 'UTC' > (now() - '{} {}s'::INTERVAL)".format(size, timeframe)
+
+    entry = db.get_single( q )
+    count = 0
+    if entry is not None:
+        count = entry['size']
+        count /= 1e9
+
+    print("data-upload,timeframe={},size=1,format=GB count={}")
+
+
+def stats_data(args):
+    if len( args.command) == 0:
+        get_upload_stats()
+        return
+
+    commands = ['month', 'day', 'hour','help']
+    command = args.command.pop( 0 )
+    args_utils.valid_command(command, commands)
+
+    size = offset = args_utils.get_or_default(args.command, 1)
+    if command == 'month':
+        get_upload_stats(month=size)
+    if command == 'day':
+        get_upload_stats(day=size)
+    if command == 'hour':
+        get_upload_stats(hour=size)
+    else:
+        print("stats users sub-commands: {}".format(", ".join(commands)))
+        sys.exit()
+
 
 def stats_command(args) -> None:
 
@@ -98,7 +186,7 @@ def stats_command(args) -> None:
     args_utils.valid_command(command, commands)
 
     if command == 'users':
-        stats_user(args)
+        stats_users(args)
     elif command == 'jobs':
         stats_jobs(args)
     elif command == 'data':
@@ -150,7 +238,8 @@ def main():
 
 
 
-
 if __name__ == "__main__":
+#    get_user_stats()
+#    get_upload_stats()
     main()
 
