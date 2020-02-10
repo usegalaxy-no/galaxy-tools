f69d0a8b58427a8c8580487da388d44228feaaca
Kidane M. Tekle <kidane.tekle@gmail.com>
2020-02-10 13:36:56 +0100

    cosmetics

--- bin/galaxy_stats.py
+++ bin/galaxy_stats.py
@@ -5,17 +5,15 @@
 #
 # Kim Brugger (03 Apr 2019), contact: kim@brugger.dk
 
-import sys
 import os
 import pprint
-import os
 import re
+import sys
 
 pp = pprint.PrettyPrinter(indent=4)
 import argparse
 
 import datetime
-
 
 import kbr.db_utils as db_utils
 import kbr.args_utils as args_utils
@@ -25,41 +23,44 @@ import kbr.file_utils as file_utils
 
 db = None
 
+
 def disk_usage():
     pass
 
-def get_job_stats(day:int=None, hour:int=None):
+
+def get_job_stats(day: int = None, hour: int = None):
     q = "SELECT state, count(*) from job "
 
     timeframe = "timeframe=epoch,"
 
     if hour is not None:
         q += "WHERE update_time > now() - INTERVAL '{} hour' ".format(hour)
-        timeframe = "timeframe=hour,size={},".format( hour )
+        timeframe = "timeframe=hour,size={},".format(hour)
     elif day is not None:
         q += "WHERE update_time > now() - INTERVAL '{} day' ".format(day)
-        timeframe = "timeframe=day,size={},".format( day )
+        timeframe = "timeframe=day,size={},".format(day)
 
     q += "GROUP BY state"
 
     total = 0
 
-    for entry in db.get_as_dict( q ):
-        print("jobs,{}state={}\tcount={}".format(timeframe,entry['state'], entry['count']))
+    for entry in db.get_as_dict(q):
+        print("jobs,{}state={}\tcount={}".format(timeframe, entry['state'], entry['count']))
         total += int(entry['count'])
 
     if total > 0:
-        print("jobs,{}state={}\tcount={}".format(timeframe,"total", total))
+        print("jobs,{}state={}\tcount={}".format(timeframe, "total", total))
+
 
 def stats_jobs(args):
-    if len( args.command) == 0:
+    if len(args.command) == 0:
         get_job_stats()
         get_job_stats(day=1)
         get_job_stats(hour=1)
         return
 
     commands = ['total', 'day', 'hour', 'help']
-    command = args.command.pop( 0 )
+    command = args.command.pop(0)
     args_utils.valid_command(command, commands)
 
     if command == 'total':
@@ -75,38 +76,37 @@ def stats_jobs(args):
         sys.exit()
 
 
-def get_user_stats(year:int=None,month:str=None):
-
+def get_user_stats(year: int = None, month: str = None):
     # default we show for the current month
     today = datetime.datetime.today()
     datem = datetime.datetime(today.year, today.month, 1)
-    where = "WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-{}-01'::date ".format( today.year, today.month )
+    where = "WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-{}-01'::date ".format(today.year,
+                                                                                                       today.month)
 
     # or show for a full year
     if year is not None:
-        where="WHERE date_trunc('year', job.create_time AT TIME ZONE 'UTC') = '{}-01-01'::date ".format( year)
+        where = "WHERE date_trunc('year', job.create_time AT TIME ZONE 'UTC') = '{}-01-01'::date ".format(year)
     # or a given month (year-month)
     elif month is not None:
-        where="WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-01'::date ".format( month )
+        where = "WHERE date_trunc('month', job.create_time AT TIME ZONE 'UTC') = '{}-01'::date ".format(month)
 
-    q  = "SELECT date_trunc('month', job.create_time AT TIME ZONE 'UTC')::date as month, "
-    q += "count(distinct user_id) AS count FROM job {}".format( where )
+    q = "SELECT date_trunc('month', job.create_time AT TIME ZONE 'UTC')::date as month, "
+    q += "count(distinct user_id) AS count FROM job {}".format(where)
     q += "GROUP BY month ORDER BY month DESC"
 
-#    print( q )
+    #    print( q )
 
-    for entry in db.get_as_dict( q ):
+    for entry in db.get_as_dict(q):
         print("active-users,timeframe=month,size=1,date={}\tcount={}".format(entry['month'], entry['count']))
 
 
-
 def stats_users(args):
-    if len( args.command) == 0:
+    if len(args.command) == 0:
         get_user_stats()
         return
 
     commands = ['year', 'month', 'help']
-    command = args.command.pop( 0 )
+    command = args.command.pop(0)
     args_utils.valid_command(command, commands)
 
     if command == 'year':
@@ -120,13 +120,12 @@ def stats_users(args):
         sys.exit()
 
 
-
 def get_queue_stats():
-    q =  "SELECT tool_id, state, count(*) as count FROM job "
+    q = "SELECT tool_id, state, count(*) as count FROM job "
     q += "WHERE state in ('queued', 'running') "
     q += "GROUP BY tool_id, state ORDER BY count desc"
 
-    for entry in db.get_as_dict( q ):
+    for entry in db.get_as_dict(q):
         entry['tool_id'] = re.sub(r'^.*repos/', '', entry['tool_id'])
         print("queue,tool_id={},state={} count={}".format(entry['tool_id'], entry['state'], entry['count']))
 
@@ -135,9 +134,8 @@ def stats_queue(args):
     get_queue_stats()
 
 
-def get_upload_stats(month:int=None,day:int=None,hour:int=None):
-
-    q  = "SELECT coalesce(sum(dataset.total_size), 0) as size FROM job "
+def get_upload_stats(month: int = None, day: int = None, hour: int = None):
+    q = "SELECT coalesce(sum(dataset.total_size), 0) as size FROM job "
     q += "LEFT JOIN job_to_output_dataset ON job.id = job_to_output_dataset.job_id "
     q += "LEFT JOIN history_dataset_association ON "
     q += " job_to_output_dataset.dataset_id = history_dataset_association.id "
@@ -145,7 +143,7 @@ def get_upload_stats(month:int=None,day:int=None,hour:int=None):
     q += "WHERE job.tool_id = 'upload1'"
 
     timeframe = "hour"
-    size      = 1
+    size = 1
     if month is not None:
         timeframe = "month"
         size = month
@@ -158,24 +156,24 @@ def get_upload_stats(month:int=None,day:int=None,hour:int=None):
 
     q += "AND job.create_time AT TIME ZONE 'UTC' > (now() - '{} {}s'::INTERVAL)".format(size, timeframe)
 
-#    print( q )
+    #    print( q )
 
-    entry = db.get_as_dict( q )
+    entry = db.get_as_dict(q)
     count = 0
     if entry is not None:
         count = float(entry[0]['size'])
         count /= 1e9
 
-    print("data-upload,timeframe={},size=1,format=GB count={}".format( timeframe, count))
+    print("data-upload,timeframe={},size=1,format=GB count={}".format(timeframe, count))
 
 
 def stats_data(args):
-    if len( args.command) == 0:
+    if len(args.command) == 0:
         get_upload_stats()
         return
 
-    commands = ['month', 'day', 'hour','help']
-    command = args.command.pop( 0 )
+    commands = ['month', 'day', 'hour', 'help']
+    command = args.command.pop(0)
     args_utils.valid_command(command, commands)
 
     size = offset = args_utils.get_or_default(args.command, 1)
@@ -191,17 +189,16 @@ def stats_data(args):
 
 
 def stats_command(args) -> None:
-
-    if len( args.command) == 0:
+    if len(args.command) == 0:
         stats_users(args)
         stats_data(args)
         stats_jobs(args)
-        stats_queue( args )
+        stats_queue(args)
         return
 
     commands = ['users', 'jobs', 'queue', 'data', 'help']
 
-    command = args.command.pop( 0 )
+    command = args.command.pop(0)
     args_utils.valid_command(command, commands)
 
     if command == 'users':
@@ -224,12 +221,11 @@ def write_config_file():
 
 
 def print_tick_entry(config_file):
-
     interpreter_path = sys.executable
     script_path = os.path.realpath(__file__)
-    config_file = os.path.abspath( config_file )
+    config_file = os.path.abspath(config_file)
 
-    cmd = "{} {} -c {}".format(interpreter_path, script_path, config_file )
+    cmd = "{} {} -c {}".format(interpreter_path, script_path, config_file)
     entry = """[[inputs.exec]]
    commands = ['{cmd} stats']
    timeout='10s'
@@ -237,13 +233,12 @@ def print_tick_entry(config_file):
    interval = '1m'
    name_prefix='galaxy_' 
 """
-    entry = entry.format( cmd=cmd )
+    entry = entry.format(cmd=cmd)
 
-    print( entry )
+    print(entry)
 
 
 def main():
-
     parser = argparse.ArgumentParser(description='cbu galaxy admin tool')
     parser.add_argument('-c', '--config', default="galaxy.json", help="config file")
 
@@ -251,7 +246,6 @@ def main():
     parser.add_argument('command', nargs='+', help="{}".format(",".join(commands)))
 
     args = parser.parse_args()
-
 
     command = args.command.pop(0)
     if command not in commands:
@@ -262,19 +256,18 @@ def main():
         print_tick_entry(args.config)
         sys.exit()
 
-    config = config_utils.readin_config_file( args.config )
+    config = config_utils.readin_config_file(args.config)
     global db
-    db = db_utils.DB( config.db_url )
+    db = db_utils.DB(config.db_url)
 
     if command == 'stats':
         stats_command(args)
     elif command == 'tick':
         print_tick_entry()
     else:
-        print("Unknown command: {} are allowed.".format(string_utils.comma_sep( commands )))
-        sys.exit( 1 )
+        print("Unknown command: {} are allowed.".format(string_utils.comma_sep(commands)))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
     main()
-
